3f66032ada88a729a00c3e144ed2ad91c2f0ab4d
tusherbhakta <tusheranta@gmail.com>
2025-01-09 12:52:12 +0530

    create header components

---
+++ src/layouts/HomeLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const HomeLayout = () => {
+    return (
+        <div>
+            <header>
+                <Header></Header>
+            </header>
+            <nav></nav>
+            <main></main>
+        </div>
+    );
+};
+
+export default HomeLayout;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
+import HomeLayout from "../layouts/HomeLayout";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element:<h1>home</h1>
+        element: <HomeLayout></HomeLayout>
     },
     {
         path: 'auth',
