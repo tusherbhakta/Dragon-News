7a941ce1954ef534c7d5ecddaf032ea117096269
tusherbhakta <tusheranta@gmail.com>
2025-01-09 17:31:26 +0530

    complete header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import logo from '../assets/logo.png'
+import moment from 'moment';
 
 const Header = () => {
     return (
-        <div>
-            this is header
+        <div className='flex flex-col justify-center items-center gap-2 py-5'>
+            <div>
+                <img className='max-w-[400px]' src={logo} alt="" />
+            </div>
+            <h1 className='text-lg text-[#706F6F]'>Journalism Without Fear or Favour</h1>
+            <p>
+                {moment().format("dddd, MMMM DD, YYYY")}
+            </p>
         </div>
     );
 };
