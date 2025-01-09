f80c4269e9880d7d6af99a3805b8c738a613edb8
tusherbhakta <tusheranta@gmail.com>
2025-01-10 02:15:09 +0530

    made structure for main section of Home page layout

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -12,12 +12,14 @@ const HomeLayout = () => {
             <section className='w-11/12 mx-auto my-9'>
                 <LatestNews></LatestNews>
             </section>
-            <section className='w-11/12 mx-auto'>
+            <nav className='w-11/12 mx-auto'>
                 <Navbar></Navbar>
-            </section>
-            <nav>
             </nav>
-            <main></main>
+            <main className='w-11/12 mx-auto grid md:grid-cols-12 pt-20 gap-3'>
+                <aside className='left col-span-3'>left navbar</aside>
+                <aside className='col-span-6'>center content</aside>
+                <aside className='col-span-3'>right navbar</aside>
+            </main>
         </div>
     );
 };
