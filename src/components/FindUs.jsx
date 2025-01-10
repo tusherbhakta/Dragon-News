dcb52e345c31d7373b5a91ebdf163a0e4586f06b
tusherbhakta <tusheranta@gmail.com>
2025-01-10 23:00:51 +0530

    complete find us component

--- src/components/FindUs.jsx
+++ src/components/FindUs.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { FaFacebook, FaInstagram } from 'react-icons/fa';
+import { FaX } from 'react-icons/fa6';
 
 const FindUs = () => {
     return (
-        <div>
-            
+        <div className='mt-10'>
+            <h2 className="text-lg font-bold">Find Us On</h2>
+            <div className="join join-vertical mt-4 w-full">
+                <button className="btn h-auto py-4 text-lg rounded-sm hover:border-blue-300 hover:text-blue-400 hover:bg-white border-slate-200 text-black-500 border join-item"><FaFacebook></FaFacebook> Facebook</button>
+                <button className="btn h-auto py-4 text-lg rounded-sm hover:border-blue-300 hover:text-blue-400 hover:bg-white border-slate-200 text-black-500 border join-item"><FaInstagram></FaInstagram> Instagram</button>
+                <button className="btn h-auto py-4 text-lg rounded-sm hover:border-blue-300 hover:text-blue-400 hover:bg-white border-slate-200 text-black-500 border join-item"><FaX></FaX> Twitter</button>
+            </div>
         </div>
     );
 };
--- src/components/layoutComponents/RightNav.jsx
+++ src/components/layoutComponents/RightNav.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import LoginWith from '../LoginWith';
+import FindUs from '../FindUs';
 
 const RightNav = () => {
     return (
         <div>
             <LoginWith></LoginWith>
+            <FindUs></FindUs>
         </div>
     );
 };
