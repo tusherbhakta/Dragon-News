0ae6361332b5cf6d384966a0f9ca8472d0b1c893
tusherbhakta <tusheranta@gmail.com>
2025-01-16 22:16:43 +0530

    login implement in LoginWith but doesn't work

--- src/components/LoginWith.jsx
+++ src/components/LoginWith.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { FaGithub, FaGoogle } from 'react-icons/fa';
+import { AuthContext } from '../provider/AuthProvider';
 
 const LoginWith = () => {
+    const { user, signUpWithGoogle } = useContext(AuthContext);
+    const handleLogInWithGoogle = (e) =>{
+        e.preventDefault();
+        signUpWithGoogle(auth, provider)
+        .then((result)=>{
+            const user= result.user;
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
+    }
     return (
         <div>
             <h2 className="font-bold text-lg">Login With</h2>
             <div className='flex flex-col mt-4 gap-2'>
-                <button className="py-2 rounded-lg hover:border-blue-300 hover:text-blue-400 hover:bg-white border-slate-400 text-black-500 border-2 btn"><FaGoogle></FaGoogle> Login with google</button>
+
+                {
+                    <button onClick={handleLogInWithGoogle}
+                    className="py-2 rounded-lg hover:border-blue-300 hover:text-blue-400 hover:bg-white border-slate-400 text-black-500 border-2 btn">
+                        <FaGoogle></FaGoogle> 
+                        Login with google
+                        </button>
+                }
+
                 <button className="py-2 rounded-lg hover:border-blue-300 hover:text-blue-400 hover:bg-white border-slate-400 text-black-500 border-2 btn"><FaGithub></FaGithub> Login with github</button>
             </div>
         </div>
